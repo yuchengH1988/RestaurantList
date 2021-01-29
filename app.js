c0a90045d3e2bcc0ba8f0b0b9431838f24dd39d9
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-01-29 11:28:47 +0800

    refactor: modify mongoosse config

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 // require packages used in the project
 const express = require('express')
 const app = express()
-const port = 3000
 const exphbs = require('express-handlebars')
-const Restaurant = require('./models/restaurant')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 const methodOverride = require('method-override')
 const routes = require('./routes')
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected')
-})
+require('./config/mongoose')
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -27,15 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
 
-
-
-// routes setting add new function
-
-
-
-
-
 // start and listen on the Express server
-app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`)
+app.listen(3000, () => {
+  console.log(`Express is listening on http://localhost:3000`)
 })
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,11 +1,8 @@
-const mongoose = require('mongoose')
+
 const Restaurant = require('../restaurant')
 const restaurantList = require('./restaurant.json')
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const db = require('../../config/mongoose')
+
 db.once('open', () => {
   console.log('mongodb connected!')
   restaurantList.results.forEach(data => {
