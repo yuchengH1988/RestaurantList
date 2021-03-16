4911c7f656f6ae2f932d93175e65f28303edb8ed
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-03-16 09:43:57 +0800

    feat:add user modules

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,13 @@
 const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
+const users = require('./modules/user')
 const restaurants = require('./modules/restaurants')
 
 // 準備引入路由模組
-
-router.use('/', home)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
+router.use('/', home)
 
 // 匯出路由器
 module.exports = router
