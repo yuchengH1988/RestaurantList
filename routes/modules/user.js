269c6a5efefb4599e57a358ffcf9978ad36d708e
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-03-16 10:32:57 +0800

    feat: add login &register page

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const router = express.Router()
+
+router.post('/login', (req, res) => {
+
+})
+
 router.get('/login', (req, res) => {
   res.render('login')
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
 })
 module.exports = router
