fbfb0f6d1e666ffd093e85c939209886e9a5b60b
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-03-17 11:49:13 +0800

    feat: add relations between User and Restaurant models

--- models/restaurant.js
+++ models/restaurant.js
@@ -11,5 +11,11 @@ const restaurantSchema = new Schema({
   google_map: String,
   rating: Number,
   description: String,
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  }
 })
 module.exports = mongoose.model('Restaurant', restaurantSchema)
--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -2,66 +2,83 @@ const express = require('express')
 const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
+//Search route
+router.get('/searchs', (req, res) => {
+  const userId = req.user._id
+  const keyword = req.query.keyword.toLowerCase()
+  console.log('keyword:', keyword)
+  return Restaurant.find({
+    userId,
+    $or: [
+      {
+        category: {
+          $regex: keyword,
+          $options: 'i'
+        }
+      },
+      {
+        name: {
+          $regex: keyword,
+          $options: 'i'
+        }
+      },
+    ]
+  })
+    .lean()
+    .then(restaurants => res.render('index', { restaurants, keyword }))
+    .catch(err => res.send(err))
+})
+
 router.get('/new', (req, res) => {
   return res.render('new')
 })
 router.post('/', (req, res) => {
+
   if (!req.body.image) { req.body.image = 'https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg' }
-  const newItem = req.body
+  let newItem = req.body
+  newItem.userId = req.user._id
   return Restaurant.create(newItem)
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
-//Search route
-router.get('/searchs', (req, res) => {
-  const keyword = req.query.keyword.toLowerCase()
-  console.log('keyword:', keyword)
-  Restaurant.find()
-    .lean()
-    .then(restaurants => {
-      return restaurants.filter(restaurant =>
-        restaurant.name.toLowerCase().includes(keyword)
-        || restaurant.category.toLowerCase().includes(keyword)
-      )
-    })
-    .then(restaurants => res.render('index', { restaurants, keyword }))
-    .catch(error => console.log(error))
-})
-
 router.get('/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then(restaurant => res.render('show', { restaurant }))
     .catch(error => console.log(error))
 })
 
 router.get('/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then(restaurant => res.render('edit', { restaurant }))
     .catch(error => console.log(error))
 })
 
 router.put('/:id', (req, res) => {
-  const id = req.params.id
-  const savedItem = req.body
-  savedItem._id = id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  let savedItem = req.body
+  savedItem._id = _id
+  return Restaurant.findOne({ userId, _id })
     .then(restaurant => {
       restaurant = Object.assign(restaurant, savedItem)
       return restaurant.save()
     })
-    .then(() => res.redirect(`/restaurants/${id}`))
+    .then(() => res.redirect(`/restaurants/${_id}`))
     .catch(error => console.log(error))
 })
 
 //Delete route
 router.delete('/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .then(restaurant => restaurant.remove())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
